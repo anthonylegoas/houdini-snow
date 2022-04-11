88a8873a115373900d399fb0c107e8c0982d8b98
Anthony Le Goas <anthony.legoas@gmail.com>
2022-04-11 11:35:09 +0200

    ✨ add html page

---
+++ properties.js
@@ -0,0 +1,20 @@
+CSS.registerProperty({
+  name: '--stars-number',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 30,
+});
+
+CSS.registerProperty({
+  name: '--extra-confettiLengthVariance',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 15,
+});
+
+CSS.registerProperty({
+  name: '--extra-confettiWeightVariance',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 4,
+});
